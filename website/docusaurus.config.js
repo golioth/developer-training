fe509dd2406dadb5fef71c1c459fd55f3ac3b8ec
Mike Szczys <mike@golioth>
2022-03-08 14:33:50 -0600

    Remove 'edit this page' links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/golioth/developer-training',
+          //editUrl: 'https://github.com/golioth/developer-training',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
