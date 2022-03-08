ddbcf43c87788cfd1a8bb65bba9707ccb7660a1e
Mike Szczys <mike@golioth>
2022-03-08 14:28:01 -0600

    fix: company name in copyright and the rendered layout

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Golioth, Inc.<br />Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
